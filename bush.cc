1913f77a4216b321ecf386d807aba3fd6b3d5f02
Jonathon Lefler <jlefler@nmu.edu>
2017-10-25 19:06:48 -0400

    Added Basic input loop

--- bush.cc
+++ bush.cc
@@ -1,7 +1,18 @@
 //Made by Jonathon Lefler and Peter Maurer
 
-#include <iostream>
+#include <iostream> //cout
+//#include <string> //string
 
 int main(int argc, char * argv[]){
-	std::cout << "Hello world" << std::endl;
+	std::string line = ""; //line will hold the entire command(cmd).
+
+	while(true){
+		std::cout << "bush~$";
+		std::getline(std::cin, line);
+		std::cout << "Command:" << line << std::endl;
+		if(line.compare("quit") == 0){
+			std::cout << "quiting" << std::endl;
+			break;
+		}
+	}
 }
