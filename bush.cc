b1157ffab999d4fdea52e01380959cd3d548d1cb
Jonathon Lefler <jlefler@nmu.edu>
2017-10-26 15:36:27 -0400

    lllll

--- bush.cc
+++ bush.cc
@@ -2,18 +2,25 @@
 
 #include <iostream> //cout
 #include <unistd.h> //getcwd
-#include <cstring> //strtok
+#include <cstring> //strtok, strlen
 #include <string> //insert
+
+int countElements(char delim, char * buf){
+	int count = 0;
+	std::cout << buf << std::endl;
+        for(int i = 0; i < strlen(buf); ++i)
+                if(buf[i] == delim)
+                        ++count; //count is the size of cwPath
+	return count;
+}
 
 int loadCwPath(std::string * &cwPath){ //uses getcwd() to load cwPath
 	char buf[100];
 	char * charPathElement;
 	getcwd(buf, 100); //gets current working directory
 
-	int count = 0;
-	for(int i = 0; i < sizeof(buf); ++i)
-		if(buf[i] == '/')
-			++count; //count is the size of cwPath
+	int count = countElements('/', buf); //count is the size of cwPath
+	std::cout << count << std::endl;
 	cwPath = new std::string[24]; //24 represents the depth of our path
 	charPathElement = strtok(buf, "/");
 	int i = 0;
@@ -34,20 +41,37 @@ void printPrompt(int cwPathLength, std::string * cwPath){
 }
 
 int main(int argc, char * argv[]){
-	std::string line = ""; //line will hold the entire command(cmd).
+	std::string line = ""; //line will hold the entire command array.
 	std::string * cwPath; //cwPath will hold the current working path
-	int cwPathLength = loadCwPath(cwPath);
-	//Will start from current working directory, can be got from loadCwPath
+	int cwPathLength = loadCwPath(cwPath); //cwPathLength will hold the
+	//current working path array size
 
 	while(true){
+		std::string * cmdArray;
+		char buf[1000];
+		char * charCmdElement;
+
 		printPrompt(cwPathLength, cwPath);
 		std::getline(std::cin, line);
-		std::cout << "Command:" << line << std::endl;
+		/*std::cout << "Command:" << line << std::endl;
+		std::strcpy(buf, line.c_str());
+		int numCmdElements = countElements(' ', buf);
+		cmdArray = new std::string[numCmdElements];
+		int i = 0;
+		charCmdElement = strtok(buf, " ");
+		while(charCmdElement != NULL){
+			std::string stringCmdElement = charCmdElement;
+			std::cout << charCmdElement << std::endl;
+			cmdArray[i] = stringCmdElement;
+			++i;
+			charCmdElement = strtok(NULL, " ");
+		}
+		std::cout << "hit" << std::endl;
 		if(line.compare("quit") == 0){ //comapre() is in std::string
 			std::cout << "quiting" << std::endl;
 			break;
 		}
 		if(line.substr(0, 2) == "cd")
 			std::cout << "You're cd'ing" << std::endl;
-	}
+	}*/
 }
